511e03c2ad497dd20b0acced149c1db4d0769ec0
bagantegianluca <bagantegianluca@gmail.com>
2023-12-05 23:09:57 +0100

    Convert in english lang and start Milestone 3

--- js/main.js
+++ js/main.js
@@ -12,30 +12,44 @@ Inserire un form per l’aggiunta di un elemento alla lista.
 Consigli del giorno: Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
 E ricordiamoci che console.log() è nostro amico! */
 
-// Chiedo all'utente quanti membri vuole inserire nel team
+// Ask user how many people the team should be
 const teamNumber = prompt('Quanti membri vuoi inserire?\nIl dato deve essere un numero da 1 a 10.');
 
 if (isNaN(teamNumber) || Number(teamNumber) < 1 || Number(teamNumber) > 10) {
     alert('Non è stato inserito un numero da 1 a 10');
 }
 
-// Creo variabile array per membri del team
+// Create variable for team members array
 const teamMembers = [];
 
-// Creo un ciclo per inserire i singoli membri del team
+// Create a loop to insert every team member in the array
 for (let i = 1; i <= Number(teamNumber); i++) {
 
-    const teamMember = prompt(`Inserisci il nome del ${i} elemento`);
+    const teamMember = prompt(`Inserisci il nome del ${i}° elemento`);
     teamMembers.push(teamMember);
 
 }
 
-// Creo un ciclo per aggiunge una card per ogni elemento dell'array
+// Create a loop to add a new card element for each team member
 for (let i = 0; i < teamMembers.length; i++) {
     newCard(teamMembers[i]);
 }
 
-//Creo una funzione che genera un elemento card e lo aggiunge alla row
+// Add an eventListener to add/remove a class to highlight (or not) the single card
+const cards = document.querySelectorAll(".card");
+const cardPressed = e => {
+    console.log(e.target.id);  // Get ID of Clicked Element
+}
+for (let card of cards) {
+    card.addEventListener("click", cardPressed);
+}
+
+
+/******************
+*FUNCTIONS SECTION*
+******************/
+
+//Create a function to generete a card element and add it to the .row element
 /**
  * ### newCard
  * Create a new card element for the person and add it to .row element
@@ -43,19 +57,19 @@ for (let i = 0; i < teamMembers.length; i++) {
  */
 function newCard(person) {
 
-    // Creo un nuovo elemento div
+    // Create a new div element
     const newElement = document.createElement('div');
 
-    // Aggiungo la classe .col-cs
+    // Add the .col-cs class
     newElement.className = 'col-cs';
 
-    // Inserisco il contenuto HTML del nuovo elemento
-    newElement.innerHTML = `<div id="${person}" class="card"><img src="https://xsgames.co/randomusers/avatar.php?g=male" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">${person}</h5><p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque veritatis nobis, aspernatur quod ab officia odit praesentium a corrupti? Facilis.</p><a href="#" class="btn btn-primary">Rimuovi</a></div></div><!-- /.card -->`;
+    // Add the new element HTML code
+    newElement.innerHTML = `<div id="${person.toLowerCase()}" class="card"><img src="https://xsgames.co/randomusers/avatar.php?g=male" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">${person}</h5><p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque veritatis nobis, aspernatur quod ab officia odit praesentium a corrupti? Facilis.</p><a href="#" class="btn btn-primary">Rimuovi</a></div></div><!-- /.card -->`;
 
-    // Seleziono l'elemento con classe .row
+    // Select the .row element
     const rowElement = document.querySelector('.row');
 
-    // Aggiungo il nuovo elemento alla fine della classe .row
+    // Add the new element at the end of the .row class
     rowElement.appendChild(newElement);
 
 }
