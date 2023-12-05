a89a0171083b6f2781f4fba05e15fd5cb966c51d
bagantegianluca <bagantegianluca@gmail.com>
2023-12-05 21:22:58 +0100

    Add HTML, CSS and JS base

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* Utilizzando i dati forniti, creare un array di stringhe con i nomi dei membri del team.
+MILESTONE 0:
+Creare l’array di stringhe contenente i nomi dei membri del team.
+MILESTONE 1:
+Stampare le informazioni su DOM come card.
+MILESTONE 3:
+Aggiungere un evento click sulla card che aggiunge/rimuove una classe per evidenziare un componente del team.
+BONUS 1:
+In generale curare la parte di UI e organizzare i singoli membri in card/schede.
+BONUS 2:
+Inserire un form per l’aggiunta di un elemento alla lista.
+Consigli del giorno: Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico! */
+
+// Creo una costante array con i membri del team
+const team = ['Gianluca', 'Marco', 'Alfredo', 'Riccardo', 'Fabio'];
+
+//Creo una funzione che genera un elemento de
